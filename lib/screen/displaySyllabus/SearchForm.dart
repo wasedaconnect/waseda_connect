3b3b3d2c884bebb21ab5e69ef767c17e5a243aa5
shohei1227 <shohei.nn2826@gmail.com>
2024-03-20 16:29:26 +0900

    make ClassListView Component

--- lib/screen/displaySyllabus/SearchForm.dart
+++ lib/screen/displaySyllabus/SearchForm.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import '../../constants/Dict.dart';
 import '../Syllabus/SyllabusSearchResult.dart';
 import 'package:waseda_connect/models/ClassModel.dart';
+import '../../components/ClassListViewComponent.dart';
 
 class SearchPage extends StatefulWidget {
   @override
@@ -82,6 +83,14 @@ class _SyllabusSearchSelectState extends State<SearchFormWithSelect> {
       SearchSelectResult = newAllSyllabusData;
     });
     print(SearchSelectResult!.length);
+
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) =>
+            SearchSelectResultPage(searchResult: SearchSelectResult!),
+      ),
+    );
   }
 
   @override
@@ -152,8 +161,6 @@ class _SyllabusSearchSelectState extends State<SearchFormWithSelect> {
             ElevatedButton(
               onPressed: () {
                 // Perform search with selectedSemester, selectedDay, and selectedPeriod
-                print(
-                    'searchClasses: $selectedSemester : $selectedDay : $selectedPeriod');
                 _searchSyllabus(selectedSemester, selectedDay, selectedPeriod);
               },
               child: Text('検索'),
@@ -166,25 +173,17 @@ class _SyllabusSearchSelectState extends State<SearchFormWithSelect> {
 }
 
 class SearchSelectResultPage extends StatelessWidget {
-  final List<String> searchResult;
+  final List<ClassModel> searchResult;
 
   const SearchSelectResultPage({required this.searchResult});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Search Results'),
-      ),
-      body: ListView.builder(
-        itemCount: searchResult.length,
-        itemBuilder: (context, index) {
-          return ListTile(
-            title: Text(searchResult[index]),
-          );
-        },
-      ),
-    );
+        appBar: AppBar(
+          title: Text('Search Results'),
+        ),
+        body: ClassListViewComponent(allSyllabusData: searchResult));
   }
 }
 
@@ -228,92 +227,11 @@ class _SyllabusSearchTextState extends State<SearchFormWithText> {
           ),
           Expanded(
             child: allSyllabusData != null && allSyllabusData!.isNotEmpty
-                ? ListView.builder(
-                    itemCount: allSyllabusData!.length, // リストのアイテム数
-                    itemBuilder: (context, index) {
-                      // 各アイテムに対するウィジェットを構築
-                      final classData = allSyllabusData![index];
-                      return SyllabusItemWidget(
-                          classData: classData); // カスタムウィジェット
-                    },
-                  )
+                ? ClassListViewComponent(allSyllabusData: allSyllabusData)
                 : Container(), // allSyllabusDataが空の場合は何も表示しない
           ),
         ],
       ),
-    );
-  }
-}
-
-class SyllabusItemWidget extends StatelessWidget {
-  final ClassModel classData;
-
-  const SyllabusItemWidget({Key? key, required this.classData})
-      : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-          child: Column(
-        children: <Widget>[
-          Card(
-              margin: const EdgeInsets.all(1.0),
-              child: InkWell(
-                child: Row(
-                  children: <Widget>[
-                    Container(
-                      padding: EdgeInsets.all(16.0),
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(12.0),
-                        boxShadow: [
-                          BoxShadow(
-                            color: Colors.grey.withOpacity(0.5),
-                            spreadRadius: 2,
-                            blurRadius: 5,
-                            offset: Offset(0, 3),
-                          ),
-                        ],
-                      ),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            classData.courseName,
-                            style: TextStyle(
-                              fontSize: 18.0,
-                              fontWeight: FontWeight.bold,
-                              color: Colors.black,
-                            ),
-                          ),
-                          SizedBox(height: 8.0),
-                          Text(
-                            classData.instructor,
-                            style: TextStyle(
-                              fontSize: 16.0,
-                              color: Colors.grey[700],
-                            ),
-                          ),
-                          SizedBox(height: 8.0),
-                          Text(
-                            '${termMap[classData.semester]} ${numToDay[classData.classDay1]} 時限(Dictまだ)${classData.classStart1}',
-                            style: TextStyle(
-                              fontSize: 16.0,
-                              color: Colors.grey[700],
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
-                onTap: () {
-                  print(classData.pKey);
-                },
-              ))
-        ],
-      )),
     );
   }
 }
